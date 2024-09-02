20a4de219b36c76f5ea210f0b32b0ced68961e99
Christophy Barth <christopherbartholomewokeke@gmail.com>
2024-09-02 15:05:53 +0100

    Code implementation. added code functionality for the smart light.

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.helloworld
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
 import com.google.android.material.materialswitch.MaterialSwitch
 
 class MainActivity : AppCompatActivity() {
@@ -11,23 +13,37 @@ class MainActivity : AppCompatActivity() {
     private lateinit var switch1 : MaterialSwitch
     private lateinit var text1: TextView
     private lateinit var name: TextView
+    private lateinit var bulb: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        card1 = findViewById(R.id.card1)
-        switch1 = findViewById(R.id.switch1)
-        text1 = findViewById(R.id.smart_tv_text)
         name = findViewById(R.id.name)
+        card1 = findViewById(R.id.card1)
+        text1 = findViewById(R.id.smart_tv_text)
+        switch1 = findViewById(R.id.switch1)
+        bulb = findViewById(R.id.bulb)
 
-        switch1.setOnCheckedChangeListener { _, isChecked ->
+        switchListener(switch1, text1, bulb, card1)
+    }
+
+    private fun switchListener(switch: MaterialSwitch, textView: TextView, imageView: ImageView, cardView: CardView){
+        switch.setOnCheckedChangeListener { _, isChecked ->
             if (isChecked){
-                card1.setCardBackgroundColor(resources.getColor(R.color.card_color_off, null))
-                text1.setTextColor(resources.getColor(R.color.white, null))
+                cardView.setCardBackgroundColor(resources.getColor(R.color.card_color_off, null))
+                textView.setTextColor(resources.getColor(R.color.white, null))
+
+                val drawable = ContextCompat.getDrawable(this, R.drawable.ic_bulb_off)
+                drawable?.setTint(ContextCompat.getColor(this, R.color.white))
+                imageView.setImageDrawable(drawable)
             }else{
-                card1.setCardBackgroundColor(resources.getColor(R.color.card_color, null))
-                text1.setTextColor(resources.getColor(R.color.black, null))
+                cardView.setCardBackgroundColor(resources.getColor(R.color.card_color, null))
+                textView.setTextColor(resources.getColor(R.color.black, null))
+
+                val drawable = ContextCompat.getDrawable(this, R.drawable.ic_bulb_off)
+                drawable?.setTint(ContextCompat.getColor(this, R.color.black))
+                imageView.setImageDrawable(drawable)
             }
         }
     }
--- app/src/main/java/com/example/helloworld/data/model/LoggedInUser.kt
+++
@@ -1,9 +0,0 @@
-package com.example.helloworld.data.model
-
-/**
- * Data class that captures user information for logged in users retrieved from LoginRepository
- */
-data class LoggedInUser(
-    val userId: String,
-    val displayName: String
-)
